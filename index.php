79a835129a6874ae5aaae668d073ec221820f4db
Marcel Leite de Farias <marcel.leitefarias@gmail.com>
2022-06-21 09:27:58 -0300

    terminando layout, estrutura html e bootstrap!

--- index.php
+++ index.php
@@ -34,32 +34,48 @@
     <div class="modal fade" id="newUserModal">
         <div class="modal-dialog">
             <div class="modal-content">
-                <div class="modal-body">
-                    <div class="row">
+                <form id="newUserForm" method="POST">
+                    <div class="modal-body">
+                        <div class="row">
 
-                        <div class="col-sm-12">
-                            <div class="form-group">
-                                <label for="first_name">Nome</label>
-                                <input type="text" class="form-control" name="first_name" id="first_name">
+                            <div class="col-sm-12">
+                                <div class="form-group">
+                                    <label for="first_name">Nome</label>
+                                    <input type="text" class="form-control" name="first_name" id="first_name">
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="col-sm-12">
-                            <div class="form-group">
-                                <label for="last_name">Sobrenome</label>
-                                <input type="text" class="form-control" name="last_name" id="last_name">
+                            <div class="col-sm-12">
+                                <div class="form-group">
+                                    <label for="last_name">Sobrenome</label>
+                                    <input type="text" class="form-control" name="last_name" id="last_name">
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="col-sm-12">
-                            <div class="form-group">
-                                <label for="gender">Gênero</label>
-                                <input type="text" class="form-control" name="gender" id="gender">
+                            <div class="col-sm-12">
+                                <div class="form-group">
+                                    <label for="gender">Gênero</label>
+                                    <select name="gender" id="gender" class="custom-select">
+                                        <option value="Male">Masculino</option>
+                                        <option value="Female">Feminino</option>
+                                    </select>
+                                </div>
                             </div>
-                        </div>
 
+                            <div class="col-sm-12">
+                                <div class="form-group">
+                                    <label for="phone">Número</label>
+                                    <input type="text" class="form-control" name="phone" id="phone">
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
-                </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">CLOSE</button>
+                        <button type="submit" class="btn btn-success">ADD NEW USER</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
@@ -67,5 +83,8 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" crossorigin="anonymous"></script>
+<script>
+    
+</script>
 </body>
 </html>
