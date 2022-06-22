e873cf6a2745a0f6a226d06933f5580c0ee4f39c
MARCEL LEITE DE FARIAS <marcel_farias@estudante.sesisenai.org.br>
2022-06-22 18:57:36 -0300

    New: Finalizando edição do crud!

--- editUser.php
+++ editUser.php
@@ -5,10 +5,11 @@ include_once 'conexaoBanco.php';
 $id = $_POST['user_id'];
 $fname = $_POST['edit_first_name'];
 $lname = $_POST['edit_last_name'];
-echo $lname;
-$gender = $row['edit_gender'];
-$phone = $row['edit_phone'];
+$gender = $_POST['edit_gender'];
+$phone = $_POST['edit_phone'];
 
+$sql = "UPDATE user_info SET `first_name` = '$fname', `last_name` = '$lname', `gender` = '$gender', `phone` = '$phone'";
+$query = $con->query($sql) or die ($con->error);
 
 
 
